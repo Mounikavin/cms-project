22c5a5da6b18b109a112b0b584bb8b5d590d6e92
Harshitha155 <harshithaa1552005@gmail.com>
2025-05-29 14:53:35 +0530

    Made changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Home from './pages/Home'
-import Developerspage from './pages/Developerspage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-       <Developerspage></Developerspage>
+       
       </div>
     </>
   )
